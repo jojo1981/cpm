b15bb9e3161e3ddb9ef4f2eb1598482ff30978e6
joost.nijhuis <jnijhuis81@gmail.com>
2013-03-12 17:50:06 +0100

    Use newer dependencies, akso change the download controller. The packages will now be retrieved from packagist.org directly.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,10 +29,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            #new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-			new JoostNijhuis\PackageManagerBundle\JoostNijhuisPackageManagerBundle()
+            new JoostNijhuis\PackageManagerBundle\JoostNijhuisPackageManagerBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
